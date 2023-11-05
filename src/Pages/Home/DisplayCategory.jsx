f1b69b8a439ae37ee6f1b49b5f597ed85360cbaa
niladhikari <niladhikari445@gmail.com>
2023-11-05 13:28:34 +0600

    add the home category of books and display this,using the use loader for the get the categaory book

---
+++ src/Pages/Home/DisplayCategory.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const DisplayCategory = ({ book }) => {
+  console.log(book);
+  const { _id,category_name, img_url } = book;
+
+  return (
+    <div>
+      <Link to={`/details/${_id}`}>
+        <div className="card px-3 md:px-0 bg-base-100 shadow-xl">
+          <figure className="">
+            <img
+              src={img_url}
+              alt="brands photo"
+              className="rounded-t-xl w-full h-72"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title text-2xl font-bold">{category_name}</h2>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+DisplayCategory.propTypes = {
+  book: PropTypes.object,
+};
+
+export default DisplayCategory;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import DisplayCategory from "./DisplayCategory";
 
 
 const Home = () => {
+    const categoryBook = useLoaderData();
+    console.log(categoryBook);
     return (
         <div>
-            <h2>Home</h2>
+            <div className="max-w-7xl mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10 mb-20">
+                {
+                   categoryBook.map(book=> <DisplayCategory key={book._id} book={book}>
+
+                   </DisplayCategory>) 
+                }
+            </div>
         </div>
     );
 };
