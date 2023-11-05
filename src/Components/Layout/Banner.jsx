a4be6d38e3e98249770d22456752d512c79fb1e0
niladhikari <niladhikari445@gmail.com>
2023-11-05 16:41:07 +0600

    add the home page banner using the swiper sider

--- src/Components/Layout/Banner.jsx
+++ src/Components/Layout/Banner.jsx
@@ -1,11 +1,57 @@
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import "./Banner.css";
 
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <Swiper
+    modules={[Navigation, Pagination, A11y]}
+    spaceBetween={50}
+    slidesPerView={1}
+    navigation
+    pagination={{ clickable: true }}
+    onSwiper={(swiper) => console.log(swiper)}
+    onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide>
+        <div className="mt-6 lg:mt-1 max-w-[1300px] mx-auto background py-4 lg:py-0 lg:h-[80vh] grid justify-center items-center">
+          <div className="text-center px-6 text-white">
+            <p className="text-2xl font-semibold mb-3">Choose Your Books!</p>
+             <h2 className="text-center text-xl md:text-6xl font-bold  md:mb-0">
+             In Books, We Find Wisdom, Adventure,Solace!
+            </h2>
+            <button className="mt-7 bg-orange-600 p-6 font-semibold text-xl">Discover Your Next Book</button>
+          </div>
         </div>
-    );
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="mt-6 lg:mt-1 max-w-[1300px] mx-auto background2 py-4 lg:py-0 lg:h-[80vh] grid justify-center items-center">
+          <div className="text-center px-6 text-white">
+            <p className="text-2xl font-semibold mb-3">Choose Your Books!</p>
+             <h2 className="text-center text-xl md:text-6xl font-bold  md:mb-0">
+             Reading A Good Book Is A Journey To Unknown Realms!
+            </h2>
+            <button className="mt-7 bg-orange-600 p-6 font-semibold text-xl">Discover Your Next Book</button>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="mt-6 lg:mt-1 max-w-[1300px] mx-auto background3 py-4 lg:py-0 lg:h-[80vh] grid justify-center items-center">
+          <div className="text-center px-6 text-white">
+            <p className="text-2xl font-semibold mb-3">Choose Your Books!</p>
+             <h2 className="text-center text-xl md:text-6xl font-bold  md:mb-0">
+             A Room Without Books Is Like A Body Without A Soul!
+            </h2>
+            <button className="mt-7 bg-orange-600 p-6 font-semibold text-xl">Discover Your Next Book</button>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,28 @@
 import { useLoaderData } from "react-router-dom";
 import DisplayCategory from "./DisplayCategory";
-
+import Banner from "../../Components/Layout/Banner";
 
 const Home = () => {
-    const categoryBook = useLoaderData();
-    console.log(categoryBook);
-    return (
-        <div>
-            <div className="max-w-7xl mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10 mb-20">
-                {
-                   categoryBook.map(book=> <DisplayCategory key={book._id} book={book}>
-
-                   </DisplayCategory>) 
-                }
-            </div>
-        </div>
-    );
+  const categoryBook = useLoaderData();
+  console.log(categoryBook);
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div>
+        <Banner></Banner>
+      </div>
+      <h2 className="text-center mt-20 text-3xl  md:text-5xl font-bold text-teal-500">
+        Books Category
+      </h2>
+      <p className="text-center px-2 md:px-0 mt-4 text-teal-500 font-semibold ">
+      The best book category is characterized by exceptional literary quality, trustworthiness, and a strong reputation for consistently delivering outstanding books and reading experiences.
+      </p>
+      <div className="max-w-7xl mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10 mb-20">
+        {categoryBook.map((book) => (
+          <DisplayCategory key={book._id} book={book}></DisplayCategory>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
