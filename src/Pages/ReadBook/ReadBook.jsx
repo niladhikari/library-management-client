054e42132f4ee348f608932ffe646611ae1a16ef
niladhikari <niladhikari445@gmail.com>
2023-12-24 11:46:52 +0600

    update Color Correction in the read book route

--- src/Pages/ReadBook/ReadBook.jsx
+++ src/Pages/ReadBook/ReadBook.jsx
@@ -6,14 +6,14 @@ const ReadBook = () => {
   return (
     <div>
       <div className="max-w-7xl mx-auto my-20">
-        <div className="px-3 md:px-0 text-purple-500 lg:flex flex-1">
+        <div className="px-3 md:px-0 text-emerald-400 lg:flex flex-1">
           <div>
             <div>
               <img src={photo} alt="" className="w-full h-[400px]" />
             </div>
             <div className=" items-center justify-around mt-2">
               <h2 className="text-xl font-bold">Book : {name}</h2>
-              <p className="font-bold text-purple-500 text-xl"> Author : {type}</p>
+              <p className="font-bold  text-xl"> Author : {type}</p>
             </div>
           </div>
           <p className="mt-4 text-lg lg:font-semibold md:px-10 flex-1">{content}</p>
